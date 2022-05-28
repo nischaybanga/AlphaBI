8d895a58ebddfe298b66c2459bc8c1f8ef39f96e
nischaybanga <banga.nischay2@gmail.com>
2022-05-28 15:55:10 +0530

    pagination done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import DisplayComponent from "./components/DisplayComponent";
 import SearchBar from "./components/SearchBar";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Pagination from "./components/Pagination";
 function App() {
   const [gifArray, setGifArray] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(3);
   useEffect(() => {
     async function fetchData() {
       setIsLoading(true);
@@ -16,8 +19,7 @@ function App() {
     }
     fetchData();
   }, []);
-  async function onSearchHandler(searchedItem){
-    //console.log(searchedItem);
+  async function onSearchHandler(searchedItem) {
     setIsLoading(true);
     const response = await axios.get(
       `https://api.giphy.com/v1/gifs/search?api_key=GlVGYHkr3WSBnllca54iNt0yFbjz7L65&limit=30&rating=g&q=${searchedItem}`
@@ -25,12 +27,20 @@ function App() {
     setGifArray(response.data.data);
     setIsLoading(false);
   }
-  //console.log(gifArray);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = gifArray.slice(indexOfFirstPost, indexOfLastPost);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div className="App">
       <SearchBar setSearch={onSearchHandler}></SearchBar>
-      {!isLoading && <DisplayComponent gifs={gifArray}></DisplayComponent>}
+      {!isLoading && <DisplayComponent gifs={currentPosts}></DisplayComponent>}
       {isLoading && <h1>loading</h1>}
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={gifArray.length}
+        paginate={paginate}
+      ></Pagination>
     </div>
   );
 }
--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,9 +1,13 @@
 function DisplayComponent(props){
-    console.log(props.gifs);
-    return(
-        <div>
-            
+    //console.log(props.gifs);
+    return (
+        <div className="gifClass">
+          {props.gifs.map((item) => {
+            return (
+                <h3>{item.username}</h3>
+            );
+          })}
         </div>
-    );
+      );
 }
 export default DisplayComponent;
